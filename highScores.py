3a39fd1a47a793673be316140428ba7911b1e8f8
budidino <budidino@gmail.com>
2019-08-16 20:46:09 +0800

    cleanup

--- highScores.py
+++ highScores.py
@@ -95,8 +95,7 @@ def updateHighScores():
 		<head>
 			<title>Multiverse VR</title>
 			<meta http-equiv="refresh" content="30" />
-			<meta name="viewport" content="width=device-width, content=height=device-height, initial-scale=1.0"> 
-			<meta name="viewport" content="width=device-width, content=height=device-height, initial-scale=1.0"> 
+			<meta name="viewport" content="width=device-width, content=height=device-height, initial-scale=1.0">
 			<link rel="stylesheet" type="text/css" href="style.css">
 		</head>
 		<body>
