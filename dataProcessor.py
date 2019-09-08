cfe09428e065a5923c0836fe9da2d06aed02555e
budidino <budidino@gmail.com>
2019-09-08 22:40:12 +0800

    added new scripts

---
+++ dataProcessor.py
@@ -0,0 +1,126 @@
+# TODO: show highscore in that song
+
+import glob #directory listing
+import json
+from pprint import pprint #pritty json print
+from operator import itemgetter #sorting
+import hashlib # hash strings (detect index.html file changes)
+import time # so we can sleep
+import datetime
+
+# github related imports and settings
+import os
+os.environ["GIT_PYTHON_REFRESH"] = "quiet"
+from git import Repo
+
+oneDriveDir = f'C:/Users/Oculus/OneDrive/'
+
+waitTime = 1.0
+
+competitionSongName = "ORIGINS"
+ignorePlayersList = ["DINO", "BAN", "BARTENDER", "KING", "PILYO", "KUNG"]
+
+fileHashCompetition = ""
+fileHashLatest = ""
+
+def git_push():
+    try:
+        repo = Repo(f'{oneDriveDir}githubProject/.git')
+        repo.git.add(update=True)
+        repo.index.commit('update from the python script')
+        origin = repo.remote(name='origin')
+        origin.push()
+    except Exception as e:
+        print('Failed to push with error: '+ str(e))
+
+
+def updateHighScores():
+    global fileHashCompetition
+    global fileHashLatest
+
+    # process score files
+    highscoresList = {}
+    latestScores = []
+    files = [f for f in glob.glob(f"{oneDriveDir}scores/*.txt")]
+
+    # TODO: check latest file timestamp - if there are changes continue generating new highscores JSON
+
+    for f in files:
+        with open(f, "r") as data_file:
+            data = json.load(data_file)
+
+            song = data["song"]
+            #data.pop('modifiers', None)
+            #data.pop('settings', None)
+
+            latestScores.append(data)
+            if song != "#unknown":
+                if song in highscoresList:
+                    highscoresList[song].append(data)
+                else:
+                    highscoresList[song] = [data]
+
+    # sort all the highscore lists
+    for s in highscoresList.keys():
+        highscoresList[s] = sorted(highscoresList[s], key=itemgetter('score'), reverse=True)
+
+    # generate HighScore list for song 'competitionSongName'
+    timeString = datetime.datetime.now().strftime("%B %d").replace(' 0', ' ')
+    print(f"{competitionSongName} - {timeString}")
+    processedPlayers = []
+    resultsCompetition = []
+    resultsCompetitionCSV = ""
+    for key, value in highscoresList.items():
+        if key.lower() == competitionSongName.lower():
+            for score in value:
+                player = score['player']
+                if player not in ignorePlayersList and player not in processedPlayers and len(player) > 1 and "difficulty" in score:
+                    resultsCompetition.append(score)
+                    processedPlayers.append(player)
+
+                    _score = score["score"]
+                    
+                    resultsCompetitionCSV += f"{player}, {}"
+
+    # save the highscore if there are scores in it
+    if len(resultsCompetition) > 0:
+        newFileJson = json.dumps(resultsCompetition, sort_keys = False, indent = 4, ensure_ascii = False)
+        hashObject = hashlib.md5(newFileJson.encode('utf-8'))
+        hashString = hashObject.hexdigest()
+
+        if fileHashCompetition != hashString:
+            print(f"Saving highscores json: {oneDriveDir}githubProject/{competitionSongName.lower()}.json")
+            fileHashCompetition = hashString
+            with open(f"{oneDriveDir}githubProject/{competitionSongName.lower()}.json", 'w', encoding='utf-8') as f:
+                f.write(newFileJson)
+
+    # sort scores by timestamp and display last few    
+    latestScores = sorted(latestScores, key=itemgetter('timestamp'), reverse=True)
+
+    # get all scores from today
+    resultsLatest = []
+    for score in latestScores:
+        dateFromTS = datetime.datetime.utcfromtimestamp(score["timestamp"]).strftime('%Y-%m-%d')
+        dateToday = datetime.datetime.today().strftime('%Y-%m-%d')
+        if dateFromTS == dateToday:
+            resultsLatest.append(score)
+
+    # save latest if there are any
+    if len(resultsLatest) > 0:
+        latestJson = json.dumps(resultsLatest, sort_keys = False, indent = 4, ensure_ascii = False)
+        latestObject = hashlib.md5(latestJson.encode('utf-8'))
+        latestHashString = latestObject.hexdigest()
+
+        if fileHashLatest != latestHashString:
+            print(f"Saving latest json: {oneDriveDir}githubProject/latest.json")
+            fileHashLatest = latestHashString
+            with open(f"{oneDriveDir}githubProject/latest.json", 'w', encoding='utf-8') as f:
+                f.write(latestJson)
+
+    # TODO: push changes if they exist
+    
+    time.sleep(waitTime)
+
+while True:
+    print("-----------------------------------------")
+    updateHighScores()
---
+++ scores.py
@@ -0,0 +1,177 @@
+# ideas: sync scores across computers (update highscore)
+# upload scores json somewhere based on device ID and them merge those two on iOS or wherever we preview data?
+# update LocalDailyLeaderboards.dat to delete lowest score in any list that has 10 songs to make sure we fetch every score
+# maybe keep only the best score
+
+import getpass #computer name
+import time
+import datetime
+import json
+import os
+from pprint import pprint #pritty json print
+
+waitTime = 1.0
+computerUser = getpass.getuser()
+scoresFolderPath = f"C:/Users/{computerUser}/OneDrive/scores/"
+#scoresFolderPath = f"C:/Users/{computerUser}/OneDrive/scoresV2/"
+
+previousGameData = {}
+previousGameScore = {}
+modifiers = {}
+settings = {}
+isFail = False
+isQuit = False
+latestFileJson = {}
+latestFilename = ""
+shouldUpdateLatestFile = False
+
+def fetchAndStoreSettings():
+    global previousGameData
+    global previousGameScore
+    global modifiers
+    global settings
+    global isFail
+    global isQuit
+    global latestFileJson
+    global latestFilename
+    global shouldUpdateLatestFile
+
+    filePlayerData = "C:\\Users\\" + computerUser + "\\AppData\\LocalLow\\Hyperbolic Magnetism\\Beat Saber\\PlayerData.dat"
+    with open(filePlayerData, "r") as data_file:
+        data = json.load(data_file)
+
+    localPlayers = data["localPlayers"][0]
+    modifiers = localPlayers["gameplayModifiers"]
+    settings = localPlayers["playerSpecificSettings"]
+    stats = localPlayers["playerAllOverallStatsData"]["partyFreePlayOverallStatsData"]
+
+    if previousGameData:
+        print("waiting for a game to finish")
+        if previousGameData["stats"]["playedLevelsCount"] and stats["playedLevelsCount"]:
+            if stats["playedLevelsCount"] - previousGameData["stats"]["playedLevelsCount"] == 1:
+                # NEW LEVEL FINISHED
+                print("LEVEL FINISHED")
+                isFail = False
+                isQuit = False
+
+                 # Check if LEVEL NOT COMPLETED
+                if previousGameData["stats"]["cleardLevelsCount"] == stats["cleardLevelsCount"]:
+                    if previousGameData["stats"]["failedLevelsCount"] != stats["failedLevelsCount"]:
+                        # LEVEL FAILED
+                        print("by FAILING!")
+                        isFail = True
+                    else:
+                        # LEVEL QUIT
+                        print("by QUITTING")
+                        isQuit = True
+                
+                previousGameScore = {
+                    "badCutsCount": stats["badCutsCount"] - previousGameData["stats"]["badCutsCount"],
+                    "cutScoreWithoutMultiplier": stats["cummulativeCutScoreWithoutMultiplier"] - previousGameData["stats"]["cummulativeCutScoreWithoutMultiplier"],
+                    "goodCutsCount": stats["goodCutsCount"] - previousGameData["stats"]["goodCutsCount"],
+                    "handDistanceTravelled": stats["handDistanceTravelled"] - previousGameData["stats"]["handDistanceTravelled"],
+                    "missedCutsCount": stats["missedCutsCount"] - previousGameData["stats"]["missedCutsCount"],
+                    "timePlayed": stats["timePlayed"] - previousGameData["stats"]["timePlayed"],
+                    "totalScore": stats["totalScore"] - previousGameData["stats"]["totalScore"]}
+
+                # SAVE BASE DATA
+                shouldUpdateLatestFile = True
+                timestamp = int(time.time())
+                player = "UNKNOWN"
+                latestFilename = f"{scoresFolderPath}{str(timestamp)}{computerUser}.txt"
+                with open(latestFilename, 'a+') as outfile:
+                    latestFileJson = {
+                        "isFail": isFail,
+                        "isQuit": isQuit,
+                        "computerName": computerUser,
+                        "player": player,
+                        "score": previousGameScore["totalScore"],
+                        "modifiers": modifiers,
+                        "settings": settings,
+                        "gameStats": previousGameScore }
+                    json.dump(latestFileJson, outfile, sort_keys = False, indent = 4, ensure_ascii = False)
+    else:
+        print("no previous stats - script just launched")
+
+    # save game info data
+    previousGameData = { "modifiers": modifiers, "settings": settings, "stats": stats }
+
+def updateLatestFileIfDataAvailable():
+    # TODO: only look for a new score if the leaderboard file was updated after latestFilename vas updated
+    fileLocalDailyLeaderboards = "C:\\Users\\" + computerUser + "\\AppData\\LocalLow\\Hyperbolic Magnetism\\Beat Saber\\LocalDailyLeaderboards.dat"
+    with open(fileLocalDailyLeaderboards, "r") as data_file:
+        data = json.load(data_file)
+
+    leaderboardsData = data["_leaderboardsData"]
+    scores = []
+
+    for item in leaderboardsData:
+        songId = item['_leaderboardId']
+        
+        # get difficulty
+        difficulty = ""
+        if songId.endswith("ExpertPlus"):
+            difficulty = "ExpertPlus"
+        elif songId.endswith("Expert"):
+            difficulty = "Expert"
+        elif songId.endswith("Hard"):
+            difficulty = "Hard"
+        elif songId.endswith("Normal"):
+            difficulty = "Normal"
+        else: 
+            difficulty = "Easy"
+
+        song = songId[: -len(difficulty)]
+
+        # add all scores
+        songScores = item["_scores"]
+        for s in songScores:
+            fullCombo = s["_fullCombo"]
+            player = s["_playerName"]
+            scoreValue = s["_score"]
+            timestamp = s["_timestamp"]
+            score = (timestamp, computerUser, player, song, difficulty, scoreValue, fullCombo)
+            scores.append(score)
+
+            fileName = f"{scoresFolderPath}{str(timestamp)}{computerUser}.txt"
+            if not os.path.exists(fileName):
+                if scoreValue == previousGameScore["totalScore"]:
+                    #delete latestFilename
+                    os.remove(latestFilename)
+
+                    global latestFileJson
+                    with open(fileName, 'a+') as outfile:
+                        jsonData = {"timestamp": timestamp,
+                                    "computerName": computerUser,
+                                    "isFail": latestFileJson["isFail"],
+                                    "isQuit": latestFileJson["isQuit"],
+                                    "player": player,
+                                    "song": song,
+                                    "difficulty": difficulty,
+                                    "score": scoreValue,
+                                    "fullCombo": fullCombo,
+                                    "modifiers": modifiers,
+                                    "settings": settings,
+                                    "gameStats": previousGameScore }
+                        json.dump(jsonData, outfile, sort_keys = False, indent = 4, ensure_ascii = False)
+
+                        global shouldUpdateLatestFile
+                        shouldUpdateLatestFile = False
+
+    # TODO: delete 10th score on all scoreboards that have 10 scores - and overwrite the local leaderboard file
+
+def fetchAndStoreScores():
+    print("-----------------------------------------------")
+    print(f"script running - {int(time.time())}")
+    fetchAndStoreSettings()
+
+    global shouldUpdateLatestFile
+
+    if shouldUpdateLatestFile:
+        updateLatestFileIfDataAvailable()
+    
+    time.sleep(waitTime)
+
+
+while True:
+    fetchAndStoreScores()
