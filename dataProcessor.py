92af856c075477df9e8fa1ad872e38f6cbd21502
budidino <budidino@gmail.com>
2019-09-11 11:42:11 +0800

    playing

--- dataProcessor.py
+++ dataProcessor.py
@@ -7,6 +7,7 @@ from operator import itemgetter #sorting
 import hashlib # hash strings (detect index.html file changes)
 import time # so we can sleep
 import datetime
+import re # string replace
 
 # github related imports and settings
 import os
@@ -78,9 +79,29 @@ def updateHighScores():
                     resultsCompetition.append(score)
                     processedPlayers.append(player)
 
-                    # TODO: CSV instead of JSON to make google charts easier
-                    #_score = score["score"]
-                    #resultsCompetitionCSV += f"{player}, {}"
+                    # CSV instead of JSON to make google charts easier
+                    cleanPlayer = re.sub(' +', ' ', player.replace(" JR", "").replace("JR ", ""))
+                    resultsCompetitionCSV += datetime.datetime.fromtimestamp(score['timestamp']).strftime('%b %d') # date
+                    resultsCompetitionCSV += f", {datetime.datetime.fromtimestamp(score['timestamp']).strftime('%I:%M')}" # time
+                    resultsCompetitionCSV += f", {cleanPlayer}"
+                    resultsCompetitionCSV += f", {score['gameStats']['goodCutsCount']}" # good cuts
+                    resultsCompetitionCSV += f", {score['gameStats']['badCutsCount'] + score['gameStats']['missedCutsCount']}" # bad+miss cuts
+                    resultsCompetitionCSV += f", {score['difficulty']}" # difficulty
+                    resultsCompetitionCSV += f", {score['score']}" # score
+                    resultsCompetitionCSV += f", {score['fullCombo']}" # FC
+                    resultsCompetitionCSV += f", {score['modifiers']['noFail']}" # no fail
+                    resultsCompetitionCSV += f", {score['modifiers']['disappearingArrows']}" # disappearingArrows
+                    resultsCompetitionCSV += f", {score['modifiers']['ghostNotes']}" # ghostNotes
+                    if score['modifiers']['songSpeed'] == 1:
+                        resultsCompetitionCSV += f", {True}" # is faster song
+                    else:
+                        resultsCompetitionCSV += f", {False}" # not faster song
+
+                    if "JR " in player or " JR" in player:
+                        resultsCompetitionCSV += f", {True}" # is junior
+                    else:
+                        resultsCompetitionCSV += f", {False}" # not junior
+                    resultsCompetitionCSV += "\n" # end of line
 
     # save the highscore if there are scores in it
     if len(resultsCompetition) > 0:
@@ -94,17 +115,48 @@ def updateHighScores():
             with open(f"{oneDriveDir}githubProject/{competitionSongName.lower()}.json", 'w', encoding='utf-8') as f:
                 f.write(newFileJson)
 
+            print(f"Saving highscores csv: {oneDriveDir}githubProject/{competitionSongName.lower()}.csv")
+            with open(f"{oneDriveDir}githubProject/{competitionSongName.lower()}.csv", 'w', encoding='utf-8') as f:
+                f.write(resultsCompetitionCSV)
+
     # sort scores by timestamp and display last few    
     latestScores = sorted(latestScores, key=itemgetter('timestamp'), reverse=True)
 
     # get all scores from today
     resultsLatest = []
+    resultsLatestCSV = ""
     for score in latestScores:
         dateFromTS = datetime.datetime.utcfromtimestamp(score["timestamp"]).strftime('%Y-%m-%d')
         dateToday = datetime.datetime.today().strftime('%Y-%m-%d')
         if dateFromTS == dateToday:
             resultsLatest.append(score)
-            # TODO: CSV instead of JSON to make google charts easier
+            
+            # CSV instead of JSON to make google charts easier
+            cleanPlayer = re.sub(' +', ' ', player)
+            pcName = '#2'
+            if score['computerName'] == "Oculus":
+                pcName = '#1'
+            songName = score['song']
+            if "custom_level" in songName:
+                songName = "custom"
+
+            resultsLatestCSV += f", {datetime.datetime.fromtimestamp(score['timestamp']).strftime('%I:%M')}" # time
+            resultsLatestCSV += f", {cleanPlayer}"
+            resultsLatestCSV += f", {pcName}"
+            resultsLatestCSV += f", {songName}"
+            resultsLatestCSV += f", {score['gameStats']['goodCutsCount']}" # good cuts
+            resultsLatestCSV += f", {score['gameStats']['badCutsCount'] + score['gameStats']['missedCutsCount']}" # bad+miss cuts
+            resultsLatestCSV += f", {score['difficulty']}" # difficulty
+            resultsLatestCSV += f", {score['score']}" # score
+            resultsLatestCSV += f", {score['fullCombo']}" # FC
+            resultsLatestCSV += f", {score['modifiers']['noFail']}" # no fail
+            resultsLatestCSV += f", {score['modifiers']['disappearingArrows']}" # disappearingArrows
+            resultsLatestCSV += f", {score['modifiers']['ghostNotes']}" # ghostNotes
+            if score['modifiers']['songSpeed'] == 1:
+                resultsLatestCSV += f", {True}" # is faster song
+            else:
+                resultsLatestCSV += f", {False}" # not faster song
+            resultsCompetitionCSV += "\n" # end of line
 
     # save latest if there are any
     if len(resultsLatest) > 0:
@@ -117,6 +169,10 @@ def updateHighScores():
             fileHashLatest = latestHashString
             with open(f"{oneDriveDir}githubProject/latest.json", 'w', encoding='utf-8') as f:
                 f.write(latestJson)
+
+            print(f"Saving latest csv: {oneDriveDir}githubProject/latest.csv")
+            with open(f"{oneDriveDir}githubProject/latest.csv", 'w', encoding='utf-8') as f:
+                f.write(resultsLatestCSV)
 
     # TODO: push changes if they exist
     
