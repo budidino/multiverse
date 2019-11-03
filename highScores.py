0be837dbfbd199bf9b0a6bd0753de7cd13edcac0
budidino <budidino@gmail.com>
2019-11-03 16:42:00 +0800

    update from the python script

--- highScores.py
+++ highScores.py
@@ -65,7 +65,7 @@ def updateHighScores():
     # generate HighScore list for song 'Overkill'
     timeString = datetime.datetime.now().strftime("%B %d - %I").replace(' 0', ' ')
     print(f"{competitionSongName} - {timeString}")
-    players = ["DINO", "BAN", "BARTENDER", "KING", "PILYO", "KUNG"]
+    players = ["DINO", "BAN", "BARTENDER", "KING", "PILYO", "KUNG", "JET", "BUDZ"]
     htmlString = ""
     htmlStringKids = ""
     for key, value in scores.items():
