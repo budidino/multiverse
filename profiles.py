371b68cccab7e0a207dc874dae1eb9ddeda38576
budidino <budidino@gmail.com>
2019-12-04 15:23:06 +0800

    update from the python script

--- profiles.py
+++ profiles.py
@@ -72,6 +72,44 @@ def getAllScores():
         except FileNotFoundError:
             print("File not found!")
             continue
+
+def topScoreHtml(score):
+    name = score['player']
+    song = score['song']
+    good = score['gameStats']['goodCutsCount']
+    bad = score['gameStats']['badCutsCount']
+    miss = score['gameStats']['missedCutsCount']
+    scoreTime = datetime.datetime.fromtimestamp(score['timestamp']).strftime("%b %d - %I:%M")
+    
+    # only if after August 18
+    modifiersHtmlString = ""
+    if score['modifiers']['energyType'] == 1:
+        modifiersHtmlString += "<img src='../../icons/battery.png' title='Battery Energy' style='width:16px; height:16px;'>"
+    if score['modifiers']['noFail']:
+        modifiersHtmlString += "<img src='../../icons/noFail.png' title='No Fail' style='width:16px; height:16px;'>"
+    if score['modifiers']['instaFail']:
+        modifiersHtmlString += "<img src='../../icons/instaFail.png' title='Insta Fail' style='width:16px; height:16px;'>"
+    if score['modifiers']['enabledObstacleType'] == 1:
+        modifiersHtmlString += "<img src='../../icons/noObstacles.png' title='No Obstacles' style='width:16px; height:16px;'>"
+    if score['modifiers']['disappearingArrows']:
+        modifiersHtmlString += "<img src='../../icons/disappearingArrows.png' title='Disappearing Arrows' style='width:16px; height:16px;'>"
+    if score['modifiers']['ghostNotes']:
+        modifiersHtmlString += "<img src='../../icons/ghostNotes.png' title='Ghost Notes' style='width:16px; height:16px;'>"
+    if score['modifiers']['noBombs']:
+        modifiersHtmlString += "<img src='../../icons/noBombs.png' title='No Bombs' style='width:16px; height:16px;'>"
+    if score['modifiers']['songSpeed'] == 1:
+        modifiersHtmlString += "<img src='../../icons/fasterSong.png' title='Faster Song' style='width:16px; height:16px;'>"
+    
+    #calculate row number and odd/even
+    rowCount += 1
+
+    classHtml = f"class='row-"
+    if rowCount % 2 == 1:
+        classHtml += "odd'"
+    else:
+        classHtml += "even'"
+
+    htmlString += f"<tr {classHtml} title='{scoreTime}'><td style='text-align: right'>{rowCount}.</td><td>{song}</td><td style='text-align: center' title='{good} / {good + bad + miss}'>{bad + miss}</td><td style='text-align: center'>{score['difficulty']}</td><td style='text-align: right'>{score['score']}</td><td style='text-align: center'>{modifiersHtmlString}</td></tr>"
     
 def processPlayerScores(name, scores):
     songsDict = defaultdict()
@@ -88,7 +126,7 @@ def processPlayerScores(name, scores):
             if score['score'] > topScore['score']:
                 topScore = score
         
-        
+
 
     # profile stats (games played, songs played, date of first game on record, date of last game on record, days played, weeks played, accuracy?, tournament wins (which month and song))
     # list best score per song and number of attempts - sort by number of attempts
@@ -97,42 +135,7 @@ def processPlayerScores(name, scores):
     htmlString = ""
     rowCount = 0
     for score in scores:
-        name = score['player']
-        song = score['song']
-        good = score['gameStats']['goodCutsCount']
-        bad = score['gameStats']['badCutsCount']
-        miss = score['gameStats']['missedCutsCount']
-        scoreTime = datetime.datetime.fromtimestamp(score['timestamp']).strftime("%b %d - %I:%M")
         
-        # only if after August 18
-        modifiersHtmlString = ""
-        if score['modifiers']['energyType'] == 1:
-            modifiersHtmlString += "<img src='../../icons/battery.png' title='Battery Energy' style='width:16px; height:16px;'>"
-        if score['modifiers']['noFail']:
-            modifiersHtmlString += "<img src='../../icons/noFail.png' title='No Fail' style='width:16px; height:16px;'>"
-        if score['modifiers']['instaFail']:
-            modifiersHtmlString += "<img src='../../icons/instaFail.png' title='Insta Fail' style='width:16px; height:16px;'>"
-        if score['modifiers']['enabledObstacleType'] == 1:
-            modifiersHtmlString += "<img src='../../icons/noObstacles.png' title='No Obstacles' style='width:16px; height:16px;'>"
-        if score['modifiers']['disappearingArrows']:
-            modifiersHtmlString += "<img src='../../icons/disappearingArrows.png' title='Disappearing Arrows' style='width:16px; height:16px;'>"
-        if score['modifiers']['ghostNotes']:
-            modifiersHtmlString += "<img src='../../icons/ghostNotes.png' title='Ghost Notes' style='width:16px; height:16px;'>"
-        if score['modifiers']['noBombs']:
-            modifiersHtmlString += "<img src='../../icons/noBombs.png' title='No Bombs' style='width:16px; height:16px;'>"
-        if score['modifiers']['songSpeed'] == 1:
-            modifiersHtmlString += "<img src='../../icons/fasterSong.png' title='Faster Song' style='width:16px; height:16px;'>"
-        
-        #calculate row number and odd/even
-        rowCount += 1
-
-        classHtml = f"class='row-"
-        if rowCount % 2 == 1:
-            classHtml += "odd'"
-        else:
-            classHtml += "even'"
-
-        htmlString += f"<tr {classHtml} title='{scoreTime}'><td style='text-align: right'>{rowCount}.</td><td>{song}</td><td style='text-align: center' title='{good} / {good + bad + miss}'>{bad + miss}</td><td style='text-align: center'>{score['difficulty']}</td><td style='text-align: right'>{score['score']}</td><td style='text-align: center'>{modifiersHtmlString}</td></tr>"
 
     # generate and save HTML file
     html = """<html>
