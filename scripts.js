d00f84db4a68d119ece55f12a4adf2566e43760e
budidino <budidino@gmail.com>
2019-12-19 16:00:33 +0800

    update from the python script

---
+++ scripts.js
@@ -0,0 +1,22 @@
+
+function openTab(evt, tabName) {
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+    document.getElementById(tabName + 'View').style.display = "block";
+    evt.currentTarget.className += " active";
+
+    localStorage.setItem('tab', tabName);
+}
+
+// Open last opened tab
+if (localStorage.getItem('tab') === null) {
+    localStorage.setItem('tab', 'Competition');
+}
+document.getElementById(localStorage.getItem('tab')).click();
