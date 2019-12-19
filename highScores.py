8170e7d81d7877338de7a8a224db8fd2deeb57a1
budidino <budidino@gmail.com>
2019-12-19 23:45:10 +0800

    update from the python script

--- highScores.py
+++ highScores.py
@@ -349,6 +349,7 @@ def generateLeaderboardHtml():
     
     # TODO:
     # Medals per player (gold, silver, bronze) based on songs score
+    # sort songs by players played
 
     global htmlStringLeaderboard
     htmlStringLeaderboard = """ <table>
