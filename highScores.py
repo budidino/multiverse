112369851bdd09749e0d3d623f6c16f521bf2d98
budidino <budidino@gmail.com>
2019-12-19 20:33:42 +0800

    update from the python script

--- highScores.py
+++ highScores.py
@@ -242,9 +242,9 @@ def processLeaderboardScores(name, scores):
         htmlSongs += topScoreHtml(pScore, rowNumber, pAttempts, player)
 
     htmlStats = ""
-    htmlStats += f"<tr class='row-even'><td style='text-align: left'>Players played</td><td style='text-align: right'>{len(playersScore)}</td></tr>"
-    htmlStats += f"<tr class='row-odd'><td style='text-align: left'>Games played</td><td style='text-align: right'>{len(scores)}</td></tr>"
-    htmlStats += f"<tr class='row-even'><td style='text-align: left'>Hours played</td><td style='text-align: right'>{str(round(timePlayed/60/60, 2))}</td></tr>"
+    htmlStats += f"<tr class='row-odd'><td style='text-align: left'>Players played</td><td style='text-align: right'>{len(playersScore)}</td></tr>"
+    htmlStats += f"<tr class='row-even'><td style='text-align: left'>Games played</td><td style='text-align: right'>{len(scores)}</td></tr>"
+    htmlStats += f"<tr class='row-odd'><td style='text-align: left'>Hours played</td><td style='text-align: right'>{str(round(timePlayed/60/60, 2))}</td></tr>"
 
     # generate and save HTML file
     html = """<html>
