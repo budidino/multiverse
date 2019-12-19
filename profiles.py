324f061ff2d3e4661e452ee33f85c58745427185
budidino <budidino@gmail.com>
2019-12-19 17:09:29 +0800

    update from the python profiles script

--- profiles.py
+++ profiles.py
@@ -140,7 +140,7 @@ def processPlayerScores(name, scores):
     htmlStats = ""
     htmlStats += f"<tr class='row-odd'><td style='text-align: left'>Games played</td><td style='text-align: right'>{len(scores)}</td></tr>"
     htmlStats += f"<tr class='row-even'><td style='text-align: left'>Songs played</td><td style='text-align: right'>{len(songsDict)}</td></tr>"
-    htmlStats += f"<tr class='row-even'><td style='text-align: left'>Hours played</td><td style='text-align: right'>{timePlayed/60/60}</td></tr>"
+    htmlStats += f"<tr class='row-even'><td style='text-align: left'>Hours played</td><td style='text-align: right'>{str(round(timePlayed/60/60, 2))}</td></tr>"
     # TODO: date of first game on record, date of last game on record, days played, weeks played, accuracy?, tournament wins (which month and song)
 
     # generate and save HTML file
