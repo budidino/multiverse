cedf1fbaab3dd719f9005f6e37fab665e5e5b2f8
budidino <budidino@gmail.com>
2019-12-20 10:08:33 +0800

    update from the python script

--- highScores.py
+++ highScores.py
@@ -147,8 +147,10 @@ def topScoreHtml(score, rowNumber, attempts, name, isPlayer = True):
         classHtml += "even'"
 
     linkedName = f"<a href='players/{slugify(name)}'>{name}</a>"
+    if not isPlayer:
+        linkedName = f"<a href='songs/{slugify(name)}'>{name}</a>"
 
-    return f"<tr {classHtml} title='{scoreTime}'><td style='text-align: center'>{attempts}</td><td style='text-align: right'>{score['score']}</td><td>{ahref}</td><td style='text-align: center' title='{good} / {good + bad + miss}'>{bad + miss}</td><td style='text-align: center'>{score['difficulty']}</td><td style='text-align: center'>{modifiersHtmlString}</td></tr>"
+    return f"<tr {classHtml} title='{scoreTime}'><td style='text-align: center'>{attempts}</td><td style='text-align: right'>{score['score']}</td><td>{linkedName}</td><td style='text-align: center' title='{good} / {good + bad + miss}'>{bad + miss}</td><td style='text-align: center'>{score['difficulty']}</td><td style='text-align: center'>{modifiersHtmlString}</td></tr>"
 
 def processPlayerScores(name, scores):
     songsDict = defaultdict()
