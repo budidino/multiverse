28ba17b202bff325f45582db06e3c98201374f56
budidino <budidino@gmail.com>
2019-12-28 21:55:12 +0800

    update from the python script

--- customSongNames.py
+++ customSongNames.py
@@ -1,6 +1,4 @@
 import hashlib
-
-hash_object = hashlib.sha1('HelWorld')
-pbHash = hash_object.hexdigest()
-length = len(pbHash.decode("hex"))
-print length
+m = hashlib.md5()
+m.update("000005fab4534d05api_key9a0554259914a86fb9e7eb014e4e5d52permswrite")
+print(m.hexdigest())
