7750a765fe95a7890df0d45acaa407077b9d0168
budidino <budidino@gmail.com>
2019-12-28 22:02:56 +0800

    update from the python script

--- customSongNames.py
+++ customSongNames.py
@@ -7,6 +7,15 @@ customSongsDir = f'C:\Program Files (x86)\Steam\steamapps\common\Beat Saber\Beat
 
 songDir = "1f90 (Overkill - Nuketime)"
 
+files = [f for f in glob.glob(f"{oneDriveDir}scores/*.txt")]
+for f in files:
+    try:
+        with open(f, "r") as data_file:
+            try:
+                data = json.load(data_file) # TODO: maybe write down which file it was in some error log file?
+            except Exception as e:
+                print(f"damaged JSON for file: {f} - {e}")
+                continue
 
 
 string = "abcdef".encode('utf-8')
